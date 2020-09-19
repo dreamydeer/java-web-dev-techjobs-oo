76e2af542b001111fa97781341a00c87a95d321b
Sofia Gloria Williamson <mushroompup@gmail.com>
2020-09-19 16:16:54 -0500

    Add 2 new tests to JobTest

--- src/Tests/JobTest.java
+++ src/Tests/JobTest.java
@@ -2,23 +2,43 @@ package Tests;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.launchcode.techjobs_oo.Job;
+import org.launchcode.techjobs_oo.*;
+
 import static org.junit.Assert.*;
 
 public class JobTest {
 
-    Job test_job_1;
-    Job test_job_2;
+    Job testJob1;
+    Job testJob2;
+    Job productTester;
 
     @Before
     public void createJobs() {
-        test_job_1 = new Job();
-        test_job_2 = new Job();
+        testJob1 = new Job();
+        testJob2 = new Job();
+        productTester = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
     }
 
     @Test
     public void testSettingJobId() {
-        assertEquals(test_job_1, test_job_2);
+        assertNotEquals(testJob1.getId(), testJob2.getId());
+        assertEquals(testJob1.getId() + 1, testJob2.getId());
     }
 
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        assertTrue(productTester.getEmployer() instanceof Employer);
+        assertEquals(productTester.getEmployer().getValue(), "ACME");
+        assertTrue(productTester.getLocation() instanceof  Location);
+        assertEquals(productTester.getLocation().getValue(), "Desert");
+        assertTrue(productTester.getPositionType() instanceof PositionType);
+        assertEquals(productTester.getPositionType().getValue(), "Quality control");
+        assertTrue(productTester.getCoreCompetency() instanceof CoreCompetency);
+        assertEquals(productTester.getCoreCompetency().getValue(), "Persistence");
+    }
+
+    @Test
+    public void testJobsForEquality() {
+        assertFalse(testJob1.equals(testJob2));
+    }
 }
